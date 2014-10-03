5a1a4413eb292c1664eadf82aeb3f8a97a126f0d
Kim Burrows <kim@shortroundgames.com>
2014-10-03 11:33:07 +0100

    test

--- node_app/recipes/setup.rb
+++ node_app/recipes/setup.rb
@@ -1,10 +1,10 @@
-#include_recipe "nodejs"
+include_recipe "nodejs"
 
 # DEPRECATED: install npm and forever package
 # include_recipe "nodejs::npm"
 # nodejs_npm "forever"
 
-#gem_package "bluepill" do
-#  version "0.0.68"
-#  action :install
-#end
+gem_package "bluepill" do
+  version "0.0.68"
+  action :install
+end
