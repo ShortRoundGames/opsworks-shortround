4f34fdd25bf8f69bd73a9e8018d670619ee0acaa
Kim Burrows <kim@shortroundgames.com>
2014-10-03 18:08:01 +0100

    - Unfixed the configure recipe.

--- node_app/recipes/configure.rb
+++ node_app/recipes/configure.rb
@@ -27,7 +27,7 @@ pillName = node[:app][layerName][:pill]
 
 
 # Determine if server is running
-status_command = "/mnt/server/current/bluepill status"
+status_command = "bluepill status"
 shell = Mixlib::ShellOut.new("#{status_command} 2>&1")
 shell.run_command
 
@@ -38,7 +38,7 @@ if shell.exitstatus == 0
     user "root"
     cwd "/tmp"
     code <<-EOS
-      /mnt/server/current/bluepill restart node1
+      bluepill restart node1
     EOS
   end
 
@@ -49,7 +49,7 @@ else
     user "root"
     cwd "/tmp"
     code <<-EOS
-      /mnt/server/current/bluepill load /mnt/server/current/#{pillName}
+      bluepill load /mnt/server/current/#{pillName}
     EOS
   end
 
