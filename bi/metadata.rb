bc0cee0f0d2fd6e8bea300a8ba8a2ee156b89982
Andy <Andy@shortroundgames.com>
2014-10-06 11:45:35 +0100

    recipe for creating a daily tasks cronjob

---
+++ bi/metadata.rb
@@ -0,0 +1,3 @@
+name "bi"
+version "0.1.0"
+
---
+++ bi/recipes/daily_tasks_cron.rb
@@ -0,0 +1,7 @@
+cron "daily_bi_tasks" do
+	"minute": "10", 
+	"hour":   "*", 
+	"month" :  "*",
+	"weekday": "*",
+	command "node /mnt/server/current/snapshots/dailyTasks >> /tmp/cron.log 2>&1"
+end
