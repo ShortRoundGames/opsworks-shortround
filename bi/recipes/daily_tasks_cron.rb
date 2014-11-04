46a82b25f7ddb6b0a5ad968e0e2d1a36b4492c37
Kim Burrows <kim@shortroundgames.com>
2014-11-04 12:59:38 +0000

    - Cron job set to 5.30 PST every day.

--- bi/recipes/daily_tasks_cron.rb
+++ bi/recipes/daily_tasks_cron.rb
@@ -1,7 +1,5 @@
 cron "daily_bi_tasks" do
-	minute "10" 
-	hour "*" 
-	month "*"
-	weekday "*"
+	minute "30" 
+	hour "5" 
 	command "/usr/local/bin/node /mnt/server/current/snapshots/dailyTasks >> /tmp/cron.log 2>&1"
 end
