ae68e33a95d76c44c21ca6e06c643b54e64eef49
Andy <Andy@shortroundgames.com>
2014-12-02 14:45:36 +0000

    first attempt at overriding the screwed bluepill file

--- sr_bluepill/recipes/setup.rb
+++ sr_bluepill/recipes/setup.rb
@@ -4,3 +4,17 @@ gem_package "bluepill" do
   version "0.0.68"
   action :install
 end
+
+# Fixup screwed version of bluepill on the instance
+if (node[:opsworks])
+  template "/usr/local/lib/ruby/gems/2.0.0/gems/bluepill-0.0.68/lib/bluepill.rb" do
+    source 'bluepill.rb.erb'
+    mode '0660'
+    user 'root'
+    group 'root'
+  end
+end
+
+
+# chmod 0755 #{install_path}/current/bluepill.rb
+# mv #{install_path}/current/bluepill.rb /usr/local/lib/ruby/gems/2.0.0/gems/bluepill-0.0.68/lib/bluepill.rb
