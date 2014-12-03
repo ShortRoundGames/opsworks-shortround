237b34a817eecc05a7e8abc35b9dc87a89031ddd
Andy <Andy@shortroundgames.com>
2014-12-03 11:03:46 +0000

    smartgit sucks

--- raknet/recipes/monit_config.rb
+++
@@ -1,8 +0,0 @@
-
-#Output the monit log file to 
-template "/etc/monit/conf.d/simple_server.monitrc" do
-    source 'simple_server.monitrc.erb'
-    owner 'root'
-    group 'root'
-    mode '0700'
-end
