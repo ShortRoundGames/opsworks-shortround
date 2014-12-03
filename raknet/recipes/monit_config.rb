9d9fa1c62bb4fa0f4544a280eaeeec85a25cb151
Andy <Andy@shortroundgames.com>
2014-12-03 09:09:39 +0000

    create a simple monit config file

--- raknet/recipes/monit_config.rb
+++ raknet/recipes/monit_config.rb
@@ -1,3 +1,8 @@
-monit_monitrc "simple_server" do
-  variables({ category: "system" })
+
+#Output the monit log file to 
+template "/etc/monit/conf.d/simple_server.monitrc" do
+    source 'simple_server.monitrc.erb'
+    owner 'root'
+    group 'root'
+    mode '0700'
 end
