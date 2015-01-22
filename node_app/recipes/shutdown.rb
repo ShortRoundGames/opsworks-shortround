d1f99a8922b0c77069ab9167396cddb528742c35
Andy <Andy@shortroundgames.com>
2015-01-22 15:56:58 +0000

    fixed up the node_app shutdown recipe

--- node_app/recipes/shutdown.rb
+++ node_app/recipes/shutdown.rb
@@ -1,11 +1,26 @@
-# Just stop the bluepill process
-bash "stop bluepill" do
-  user "root"
-  cwd "/tmp"
-  code <<-EOS
-    /opt/chef/embedded/bin/bluepill stop node1
-  EOS
+# Find the attributes for this layer
+attribs = "";
+if (node[:opsworks])
+  node["opsworks"]["instance"]["layers"].each do |layerName|
+    if (node[:app][layerName])
+      attribs = node[:app][layerName]
+	end
+  end
 end
 
+# Find the app name
+app_name = attribs[:app_name];
+if (!app_name)
+	app_name = ""
+end
+
+# Send the stop to the pill 
+bash "stop bluepill" do
+    user "root"
+    cwd "/tmp"
+    code <<-EOS
+      bluepill #{app_name} stop
+    EOS
+end
 
 log "That's all folks!"
