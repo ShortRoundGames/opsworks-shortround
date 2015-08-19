893cb22482465002bd80073e56dd65791a630d3c
Kim Burrows <kim@shortroundgames.com>
2015-08-19 11:37:44 +0100

    - Rejigged cookbooks for mongodb.

---
+++ mongodb-aws/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-aws"
+description 'Cookbook for MongoDB Berkshelf based install'
+maintainer  "ShortRound Games"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
