b737d48e8299dd55972a92938ee720efcef1b0e1
Kim Burrows <kim@shortroundgames.com>
2015-08-20 16:06:44 +0100

    - Start of script to fire up a test mongo server.

--- mongodb-tanks/attributes/default.rb
+++ mongodb-tanks/attributes/default.rb
@@ -1,15 +1,22 @@
 
+default[:mongodb][:user] = "mongodb"
+default[:mongodb][:group] = "mongodb"
+default[:mongodb][:root_group] = "root"
+
 case node['platform_family']
 when "freebsd"
+  default[:mongodb][:root_group] = "wheel"
   default[:mongodb][:package_name] = "mongodb"
 
 when "rhel","fedora"
   default[:mongodb][:package_name] = "mongo-10gen-server"
 
 else
+  default[:mongodb][:root_group] = "root"
   default[:mongodb][:package_name] = "mongodb-10gen"
   default[:mongodb][:apt_repo] = "debian-sysvinit"
 
 end
 
 default[:mongodb][:package_version] = nil
+default[:mongodb][:template_cookbook] = "mongodb-tanks"
--- mongodb-tanks/recipes/default.rb
+++ mongodb-tanks/recipes/default.rb
@@ -6,3 +6,62 @@ package node[:mongodb][:package_name] do
   action :install
   version node[:mongodb][:package_version]
 end
+
+
+## Setup Static Data server
+
+# create config file
+template "/etc/mongo_static_1.conf" do
+  action :create
+  cookbook node['mongodb']['template_cookbook']
+  source "mongo.conf.erb"
+  group node['mongodb']['root_group']
+  owner "root"
+  mode "0644"
+  variables(
+    "port" => 27017,
+    "logpath" => "/mnt/mongo_static_1.log",
+    "dbpath" => "/mnt/mongo_static_1",
+    "replicaset_name" => "tanks"
+  )
+  notifies :restart, "service[#{name}]"
+end
+
+# dbpath dir [make sure it exists]
+directory "/mnt/mongo_static_1" do
+  owner node[:mongodb][:user]
+  group node[:mongodb][:group]
+  mode "0755"
+  action :create
+  recursive true
+end
+
+# Start the mongo daemon
+bash "start mongod" do
+  user "root"
+  cwd "/mnt"
+  code <<-EOS
+	mongod -f /etc/mongo_static_1.conf
+  EOS
+end
+
+#KIMTODO
+#- download static data from s3
+#- import static data into mongod
+#- delete temp downloaded data
+#- rs.initiate()
+
+
+# Setup Config Servers
+#KIMTODO
+#- write 3 config files
+#- create 3 dbpaths
+#- start 3 mongod's
+
+
+# Setup UserData shard
+#KIMTODO
+#- write config file
+#- create dbpath
+#- start mongod instance
+#- rs.initiate()
