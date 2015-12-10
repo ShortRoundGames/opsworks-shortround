02e344c60f4484217d979de98076858ed3f0fcc1
Kim Burrows <kim@shortroundgames.com>
2015-12-10 16:34:15 +0000

    - Setup daily retention cron job.

--- bi/recipes/daily_tasks_cron.rb
+++ bi/recipes/daily_tasks_cron.rb
@@ -3,3 +3,9 @@ cron "daily_bi_tasks" do
 	hour "5" 
 	command "/usr/local/bin/node /mnt/server/current/snapshots/dailyTasks >> /tmp/cron.log 2>&1"
 end
+
+cron "daily_retention" do
+	minute "0" 
+	hour "1" 
+	command "/usr/local/bin/node /mnt/server/current/retention/calculateRetention >> /tmp/cron.log 2>&1"
+end
