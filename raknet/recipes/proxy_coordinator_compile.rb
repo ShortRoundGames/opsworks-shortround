84685b3597520c45b0dccdf101da13206e3cb9c4
Andy <Andy@shortroundgames.com>
2016-02-09 12:52:54 +0000

    moved the source code into a "source" directory

--- raknet/recipes/proxy_coordinator_compile.rb
+++ raknet/recipes/proxy_coordinator_compile.rb
@@ -2,11 +2,10 @@
 # Compile
 bash "compile proxy_coordinator" do
   user "root"
-  cwd "/mnt/raknet/Source"
+  cwd "/mnt/raknet/source/Source"
 
   code <<-EOS
-    mkdir -p ../bin
-	mkdir -p ../logs
-    g++ -o ../bin/proxy_coordinator.out -pthread -I./ -I../Samples/CloudServer ../Samples/CloudServer/CloudServerHelper.cpp ../Samples/UDPProxyCoordinator/main.cpp *.cpp
+    mkdir -p /mnt/raknet/bin
+    g++ -o /mnt/raknet/bin/proxy_coordinator.out -pthread -I./ -I../Samples/CloudServer ../Samples/CloudServer/CloudServerHelper.cpp ../Samples/UDPProxyCoordinator/main.cpp *.cpp
   EOS
 end
