714fbba6eac984f84a993efa4947fd98652c0b8a
Andy <Andy@shortroundgames.com>
2016-02-09 13:27:06 +0000

    pills for the proxy server

---
+++ raknet/recipes/proxy_server_bluepill.rb
@@ -0,0 +1,7 @@
+
+template "/mnt/raknet/bin/proxy_server.pill" do
+    source 'proxy_server.pill.erb'
+    owner 'root'
+    group 'root'
+    mode '0700'
+end
--- raknet/recipes/proxy_server_compile.rb
+++ raknet/recipes/proxy_server_compile.rb
@@ -2,10 +2,10 @@
 # Compile
 bash "compile proxy_server" do
   user "root"
-  cwd "/mnt/raknet/Source"
+  cwd "/mnt/raknet/source/Source"
 
   code <<-EOS
-    mkdir -p ../bin
-    g++ -o ../bin/proxy_server.out -pthread -I./ -I../Samples/CloudServer ../Samples/CloudServer/CloudServerHelper.cpp ../Samples/UDPProxyServer/main.cpp *.cpp
+    mkdir -p /mnt/raknet/bin
+    g++ -o /mnt/raknet/bin/proxy_server.out -pthread -I./ -I../Samples/CloudServer ../Samples/CloudServer/CloudServerHelper.cpp ../Samples/UDPProxyServer/main.cpp *.cpp
   EOS
 end
