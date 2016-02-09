0c89c0da90db82c0104c48991081b5df5c5a3462
Kim Burrows <kim@shortroundgames.com>
2016-02-09 11:33:46 +0000

    - Fixed up bluepill recipe.

--- sr_bluepill/recipes/setup.rb
+++ sr_bluepill/recipes/setup.rb
@@ -1,20 +1,22 @@
 
+# install a specific version of this dependency, that we know works ok with Ruby 2.0.0
+gem_package "activesupport" do
+  version "4.2.1"
+  action :install
+end
+
 # install bluepill
 gem_package "bluepill" do
-  version "0.0.68"
+  version "0.1.1"
   action :install
 end
 
 # Fixup screwed version of bluepill on the instance
-if (node[:opsworks])
-  template "/usr/local/lib/ruby/gems/2.0.0/gems/bluepill-0.0.68/lib/bluepill.rb" do
-    source 'bluepill.rb.erb'
-    mode '0755'
-    user 'root'
-    group 'root'
-  end
-end
-
-
-# chmod 0755 #{install_path}/current/bluepill.rb
-# mv #{install_path}/current/bluepill.rb /usr/local/lib/ruby/gems/2.0.0/gems/bluepill-0.0.68/lib/bluepill.rb
+#if (node[:opsworks])
+#  template "/usr/local/lib/ruby/gems/2.0.0/gems/bluepill-0.0.68/lib/bluepill.rb" do
+#    source 'bluepill.rb.erb'
+#    mode '0755'
+#    user 'root'
+#    group 'root'
+#  end
+#end
