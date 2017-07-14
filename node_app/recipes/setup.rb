20c4e5cf266b1023f2f444809c380623d68fa460
Kim Burrows <kim@shortroundgames.com>
2017-07-14 14:21:20 +0100

    - Node app setup recipe now installs GCC 5.x.

--- node_app/recipes/setup.rb
+++ node_app/recipes/setup.rb
@@ -13,3 +13,17 @@ gem_package "bluepill" do
   version "0.1.1"
   action :install
 end
+
+# Install GCC 5.x (required to build some Node modules under Node 6)
+bash "install gcc 5.x" do
+  user "root"
+  cwd "/tmp"
+
+  code <<-EOS
+    add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    apt-get update
+    apt-get -y install gcc-5 g++-5
+    rm /usr/bin/g++
+    ln -s /usr/bin/g++-5 /usr/bin/g++
+  EOS
+end
