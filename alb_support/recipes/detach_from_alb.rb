09377186e9345040407ddcb2438f2824d77c102c
Andy <andy@shortroundgames.com>
2021-02-04 15:37:13 +0000

    first fix

--- alb_support/recipes/detach_from_alb.rb
+++ alb_support/recipes/detach_from_alb.rb
@@ -59,22 +59,17 @@ ruby_block "detach from ALB" do
 		Chef::Log.info("Deregistering EC2 instance #{ec2_instance_id} from Target Group #{target_group_arn}")
 		client.deregister_targets(target_to_detach)
 	end
+    
+	start_time = Time.now
 
-    if connection_draining_timeout == 0
-      Chef::Log.info("connection_draining_timeout was set to 0 seconds. execution of shutdown recipes will not be delayed")
-    else
-      Chef::Log.info("delaying execution recipes until instance is drained from ALB or timeout of #{connection_draining_timeout} seconds elapses")
-      start_time = Time.now
-      
-	  targets.each do |target_to_detach|	
-
+	targets.each do |target_to_detach|	
 		connection_draining_timeout = target_to_detach[:connection_draining_timeout]
 		state_check_frequency = target_to_detach[:state_check_frequency]
 
 		Chef::Log.info("DRAINING TARGET GROUP: #{target_to_detach[:target_group_arn]}")
 		Chef::Log.info("connection_draining_timeout: #{connection_draining_timeout}")
 		Chef::Log.info("state_check_frequency: #{state_check_frequency}")
-	  
+
 		loop do
 			response = client.describe_target_health(target_to_detach)
 			target_health_state = response[:target_health_descriptions].first[:target_health][:state]
@@ -85,8 +80,8 @@ ruby_block "detach from ALB" do
 			break if target_health_state == "unused" || seconds_elapsed > connection_draining_timeout
 			sleep(state_check_frequency)
 		  end
-	  end	  
-    end
-  end
-  action :run
+		end	  
+
+	end
+	action :run
 end
