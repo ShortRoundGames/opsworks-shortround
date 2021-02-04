630f5146fe5114a4d5ec4368fa7b41c7837f2a2a
Andy <andy@shortroundgames.com>
2021-02-04 08:51:59 +0000

    first attempt at making the alb data a list

--- alb_support/recipes/attach_to_alb.rb
+++ alb_support/recipes/attach_to_alb.rb
@@ -32,19 +32,23 @@ ruby_block "attach to ALB" do
 
     stack_region = stack[:region]
     ec2_instance_id = instance[:ec2_instance_id]
-    target_group_arn = node[:alb_helper][:target_group_arn]
+	
+	node[:alb_helper].each do |target_group_arn|
+		Chef::Log.info("Creating ELB client in region #{stack_region}")
+		client = Aws::ElasticLoadBalancingV2::Client.new(region: stack_region)
 
-    Chef::Log.info("Creating ELB client in region #{stack_region}")
-    client = Aws::ElasticLoadBalancingV2::Client.new(region: stack_region)
+		Chef::Log.info("Registering EC2 instance #{ec2_instance_id} with target group #{target_group_arn}")
 
-    Chef::Log.info("Registering EC2 instance #{ec2_instance_id} with target group #{target_group_arn}")
+		target_to_attach = {
+		  target_group_arn: target_group_arn,
+		  targets: [{ id: ec2_instance_id }]
+		}
 
-    target_to_attach = {
-      target_group_arn: target_group_arn,
-      targets: [{ id: ec2_instance_id }]
-    }
+		client.register_targets(target_to_attach)
+		
+	end
 
-    client.register_targets(target_to_attach)
+    
   end
   action :run
 end
