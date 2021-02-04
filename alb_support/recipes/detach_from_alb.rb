6b11b4707824de1b7ecaf29228417e5646f32587
Andy <andy@shortroundgames.com>
2021-02-04 16:11:45 +0000

    missing end

--- alb_support/recipes/detach_from_alb.rb
+++ alb_support/recipes/detach_from_alb.rb
@@ -16,71 +16,72 @@
 # language governing permissions and limitations under the License.
 #
 ruby_block "detach from ALB" do
-  block do
-    require "aws-sdk-core"
+	block do
+		require "aws-sdk-core"
 
-    raise "alb_helper block not specified in layer JSON" if node[:alb_helper].nil?
-     
-    stack = search("aws_opsworks_stack").first
-    instance = search("aws_opsworks_instance", "self:true").first
+		raise "alb_helper block not specified in layer JSON" if node[:alb_helper].nil?
+		 
+		stack = search("aws_opsworks_stack").first
+		instance = search("aws_opsworks_instance", "self:true").first
 
-    stack_region = stack[:region]
-    ec2_instance_id = instance[:ec2_instance_id]
+		stack_region = stack[:region]
+		ec2_instance_id = instance[:ec2_instance_id]
 
-    Chef::Log.info("Creating ELB client in region #{stack_region}")
-    client = Aws::ElasticLoadBalancingV2::Client.new(region: stack_region)
+		Chef::Log.info("Creating ELB client in region #{stack_region}")
+		client = Aws::ElasticLoadBalancingV2::Client.new(region: stack_region)
 
-	targets = []
+		targets = []
 
-	node[:alb_helper].each do |target_group_data|
-	
-		raise "Target Group ARN not specified in layer JSON" if target_group_data[:target_group_arn].nil?
+		node[:alb_helper].each do |target_group_data|
 		
-		target_group_arn = target_group_data[:target_group_arn]
-		connection_draining_timeout = target_group_data[:connection_draining_timeout]
-		state_check_frequency = target_group_data[:state_check_frequency]
-		
-		if (connection_draining_timeout.nil)
-			connection_draining_timeout = 750
+			raise "Target Group ARN not specified in layer JSON" if target_group_data[:target_group_arn].nil?
+			
+			target_group_arn = target_group_data[:target_group_arn]
+			connection_draining_timeout = target_group_data[:connection_draining_timeout]
+			state_check_frequency = target_group_data[:state_check_frequency]
+			
+			if (connection_draining_timeout.nil)
+				connection_draining_timeout = 750
+			end
+			
+			if (state_check_frequency.nil)
+				state_check_frequency = 30	
+			end		
+			
+			target_to_detach = {
+			  target_group_arn: target_group_arn,
+			  targets: [ { id: ec2_instance_id } ],
+			  connection_draining_timeout: connection_draining_timeout,
+			  state_check_frequency: state_check_frequency
+			}
+			
+			targets.push(target_to_detach)
+
+			Chef::Log.info("Deregistering EC2 instance #{ec2_instance_id} from Target Group #{target_group_arn}")
+			client.deregister_targets(target_to_detach)
 		end
 		
-		if (state_check_frequency.nil)
-			state_check_frequency = 30	
+		start_time = Time.now
+
+		targets.each do |target_to_detach|	
+			connection_draining_timeout = target_to_detach[:connection_draining_timeout]
+			state_check_frequency = target_to_detach[:state_check_frequency]
+
+			Chef::Log.info("DRAINING TARGET GROUP: #{target_to_detach[:target_group_arn]}")
+			Chef::Log.info("connection_draining_timeout: #{connection_draining_timeout}")
+			Chef::Log.info("state_check_frequency: #{state_check_frequency}")
+
+			loop do
+				response = client.describe_target_health(target_to_detach)
+				target_health_state = response[:target_health_descriptions].first[:target_health][:state]
+				Chef::Log.info("state of instance in ALB: #{target_health_state}")
+				seconds_elapsed = Time.now - start_time
+				Chef::Log.info("#{seconds_elapsed} of a maximum #{connection_draining_timeout} seconds elapsed")
+				Chef::Log.info("Sleeping #{ state_check_frequency} seconds")
+				break if target_health_state == "unused" || seconds_elapsed > connection_draining_timeout
+				sleep(state_check_frequency)
+			 end
 		end		
-		
-		target_to_detach = {
-		  target_group_arn: target_group_arn,
-		  targets: [ { id: ec2_instance_id } ],
-		  connection_draining_timeout: connection_draining_timeout,
-		  state_check_frequency: state_check_frequency
-		}
-		
-		targets.push(target_to_detach)
-
-		Chef::Log.info("Deregistering EC2 instance #{ec2_instance_id} from Target Group #{target_group_arn}")
-		client.deregister_targets(target_to_detach)
-	end
-    
-	start_time = Time.now
-
-	targets.each do |target_to_detach|	
-		connection_draining_timeout = target_to_detach[:connection_draining_timeout]
-		state_check_frequency = target_to_detach[:state_check_frequency]
-
-		Chef::Log.info("DRAINING TARGET GROUP: #{target_to_detach[:target_group_arn]}")
-		Chef::Log.info("connection_draining_timeout: #{connection_draining_timeout}")
-		Chef::Log.info("state_check_frequency: #{state_check_frequency}")
-
-		loop do
-			response = client.describe_target_health(target_to_detach)
-			target_health_state = response[:target_health_descriptions].first[:target_health][:state]
-			Chef::Log.info("state of instance in ALB: #{target_health_state}")
-			seconds_elapsed = Time.now - start_time
-			Chef::Log.info("#{seconds_elapsed} of a maximum #{connection_draining_timeout} seconds elapsed")
-			Chef::Log.info("Sleeping #{ state_check_frequency} seconds")
-			break if target_health_state == "unused" || seconds_elapsed > connection_draining_timeout
-			sleep(state_check_frequency)
-		 end
 	end
 	action :run
 end
